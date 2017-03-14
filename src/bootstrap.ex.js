9060167b2119a9844d07a43f860f10eb0ec86c87
Jo Kerkhofs <jke@thinkerit.be>
2017-03-14 16:58:20 +0100

    Added binCols component to easily add clearfix classes when needed

--- src/bootstrap.ex.js
+++ src/bootstrap.ex.js
@@ -1,6 +1,7 @@
 (function () {
     angular.module('bootstrap.ex', [])
-        .service('binModal', ['$rootScope', '$document', '$compile', '$templateCache', binModalService]);
+        .service('binModal', ['$rootScope', '$document', '$compile', '$templateCache', binModalService])
+        .component('binCols', new BinColsComponent());
 
     function binModalService($rootScope, $document, $compile, $templateCache) {
         var self = this, scope, element, isOpen, isClosing, openDeferred;
@@ -42,5 +43,50 @@
             if (openDeferred) openDeferred();
             openDeferred = undefined;
         }
+    }
+
+    function BinColsComponent() {
+        this.template = '<div ng-class="$ctrl.clearfixClass"></div><div ng-class="$ctrl.cssClass" ng-transclude></div>';
+        this.transclude = true;
+        this.bindings = {
+            index: '<',
+            cols: '@'
+        };
+        this.controller = function () {
+            this.$onInit = function () {
+                var xs, sm, md, lg, i = this.index;
+                angular.forEach(this.cols.split(' '), function (v) {
+                    if (v.indexOf('xs') != -1) xs = extractInt(v);
+                    if (v.indexOf('sm') != -1) sm = extractInt(v);
+                    if (v.indexOf('md') != -1) md = extractInt(v);
+                    if (v.indexOf('lg') != -1) lg = extractInt(v);
+                });
+                this.cssClass = getCssClass();
+                this.clearfixClass = getClearfixClass();
+
+                function extractInt(v) {
+                    return v.match(/[\d]+/)[0];
+                }
+
+                function getCssClass() {
+                    var c = '';
+                    if (xs) c += ' col-xs-' + xs;
+                    if (sm) c += ' col-sm-' + sm;
+                    if (md) c += ' col-md-' + md;
+                    if (lg) c += ' col-lg-' + lg;
+                    return c.trim();
+                }
+    
+                function getClearfixClass() {
+                    var visibleXsBlock = xs && i % (12/xs) == 0 ? ' visible-xs-block' : '';
+                    var visibleSmBlock = sm && i % (12/sm) == 0 ? ' visible-sm-block' : '';
+                    var visibleMdBlock = md && i % (12/md) == 0 ? ' visible-md-block' : '';
+                    var visibleLgBlock = lg && i % (12/lg) == 0 ? ' visible-lg-block' : '';
+                    var c = (visibleXsBlock + visibleSmBlock + visibleMdBlock + visibleLgBlock).trim();
+                    if (c != '') c = 'clearfix ' + c;
+                    return c;
+                }
+            };
+        };
     }
 })();
--- test/bootstrap.ex.spec.js
+++ test/bootstrap.ex.spec.js
@@ -185,4 +185,127 @@ describe('bootstrap.ex', function () {
             });
         });
     });
+
+    describe('binCols component', function () {
+        var $ctrl;
+
+        beforeEach(inject(function ($componentController) {
+            $ctrl = $componentController('binCols');
+        }));
+
+        [
+            {actual: '', expected: ''},
+            {actual: 'xs-12', expected: 'col-xs-12'},
+            {actual: 'xs-12 sm-6', expected: 'col-xs-12 col-sm-6'},
+            {actual: 'xs-12 sm-6 md-4', expected: 'col-xs-12 col-sm-6 col-md-4'},
+            {actual: 'xs-12 sm-6 md-4 lg-3', expected: 'col-xs-12 col-sm-6 col-md-4 col-lg-3'},
+            {actual: 'col-xs-12 col-sm-6 col-md-4 col-lg-3', expected: 'col-xs-12 col-sm-6 col-md-4 col-lg-3'},
+            {actual: 'xs-12 md-4 lg-3', expected: 'col-xs-12 col-md-4 col-lg-3'},
+            {actual: 'xs-12 lg-3', expected: 'col-xs-12 col-lg-3'}
+        ].forEach(function (test) {
+            describe('when cols are set to "' + test.actual + '"', function () {
+                beforeEach(function () {
+                    $ctrl.cols = test.actual;
+                    $ctrl.$onInit();
+                });
+
+                it('cssClass is available', function () {
+                    expect($ctrl.cssClass).toEqual(test.expected);
+                });
+            });
+        });
+
+        describe('with cols', function () {
+            beforeEach(function () {
+               $ctrl.cols = 'xs-12 sm-6 md-4 lg-3'
+            });
+
+            [
+                {index: 0, expected: 'clearfix visible-xs-block visible-sm-block visible-md-block visible-lg-block'},
+                {index: 1, expected: 'clearfix visible-xs-block'},
+                {index: 2, expected: 'clearfix visible-xs-block visible-sm-block'},
+                {index: 3, expected: 'clearfix visible-xs-block visible-md-block'},
+                {index: 4, expected: 'clearfix visible-xs-block visible-sm-block visible-lg-block'},
+                {index: 5, expected: 'clearfix visible-xs-block'},
+                {index: 6, expected: 'clearfix visible-xs-block visible-sm-block visible-md-block'},
+                {index: 7, expected: 'clearfix visible-xs-block'},
+                {index: 8, expected: 'clearfix visible-xs-block visible-sm-block visible-lg-block'},
+                {index: 9, expected: 'clearfix visible-xs-block visible-md-block'},
+                {index: 10, expected: 'clearfix visible-xs-block visible-sm-block'}
+            ].forEach(function (test) {
+                describe('and index is: ' + test.index, function () {
+                    beforeEach(function () {
+                        $ctrl.index = test.index;
+                        $ctrl.$onInit();
+                    });
+
+                    it('clearfixClass is available', function () {
+                        expect($ctrl.clearfixClass).toEqual(test.expected);
+                    });
+                });
+            });
+        });
+
+        describe('with cols', function () {
+            beforeEach(function () {
+               $ctrl.cols = 'xs-6 sm-4'
+            });
+
+            [
+                {index: 0, expected: 'clearfix visible-xs-block visible-sm-block'},
+                {index: 1, expected: ''},
+                {index: 2, expected: 'clearfix visible-xs-block'},
+                {index: 3, expected: 'clearfix visible-sm-block'},
+                {index: 4, expected: 'clearfix visible-xs-block'},
+                {index: 5, expected: ''},
+                {index: 6, expected: 'clearfix visible-xs-block visible-sm-block'},
+                {index: 7, expected: ''},
+                {index: 8, expected: 'clearfix visible-xs-block'},
+                {index: 9, expected: 'clearfix visible-sm-block'},
+                {index: 10, expected: 'clearfix visible-xs-block'}
+            ].forEach(function (test) {
+                describe('and index is: ' + test.index, function () {
+                    beforeEach(function () {
+                        $ctrl.index = test.index;
+                        $ctrl.$onInit();
+                    });
+
+                    it('clearfixClass is available', function () {
+                        expect($ctrl.clearfixClass).toEqual(test.expected);
+                    });
+                });
+            });
+        });
+
+        describe('with cols', function () {
+            beforeEach(function () {
+               $ctrl.cols = 'sm-4'
+            });
+
+            [
+                {index: 0, expected: 'clearfix visible-sm-block'},
+                {index: 1, expected: ''},
+                {index: 2, expected: ''},
+                {index: 3, expected: 'clearfix visible-sm-block'},
+                {index: 4, expected: ''},
+                {index: 5, expected: ''},
+                {index: 6, expected: 'clearfix visible-sm-block'},
+                {index: 7, expected: ''},
+                {index: 8, expected: ''},
+                {index: 9, expected: 'clearfix visible-sm-block'},
+                {index: 10, expected: ''}
+            ].forEach(function (test) {
+                describe('and index is: ' + test.index, function () {
+                    beforeEach(function () {
+                        $ctrl.index = test.index;
+                        $ctrl.$onInit();
+                    });
+
+                    it('clearfixClass is available', function () {
+                        expect($ctrl.clearfixClass).toEqual(test.expected);
+                    });
+                });
+            });
+        });
+    });
 });
